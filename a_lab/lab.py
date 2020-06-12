3b99a712dc29e61acb2d00ef26f2413fbacb4d5b
Kevin Jin <kjin2010@gmail.com>
2020-06-12 01:51:09 -0500

    add tokenizing

--- a_lab/lab.py
+++ a_lab/lab.py
@@ -1,18 +1,48 @@
 from enum import Enum
 import re
 
-class Token(Enum):
-    LEFT_P = '('
-    RIGHT_P = ')'
+# splits a program into its tokens 
+def tokenize(program):
+    search_words = '(\s|=|\(|\)|;)'
+    dirty_split = re.split(search_words, program)
+    repetitions = ['', ' ']
+    # removing spaces and blank tokens
+    for token in repetitions:
+        while token in dirty_split:
+            dirty_split.remove(token)
+    # function to clear tokens from every ; to \n
+    remove_comments(dirty_split)
+    while '\n' in dirty_split:
+        dirty_split.remove('\n')
+    return dirty_split
 
-left_p = '('
-if (left_p == Token.LEFT_P.value):
-    print('true')
+# helper function that removes comment tokens
+def remove_comments(tokens):
+    is_comment = False
+    i = 0
+    while i < len(tokens):
+        # if comment
+        if is_comment:
+            # end of comment
+            if tokens[i] == '\n':
+                is_comment = False
+            # delete regardless if end of comment or not 
+            del(tokens[i])
+        # if start of comment
+        elif tokens[i] == ';':
+            is_comment = True
+            del(tokens[i])
+        # if not comment or start of comment, increment index
+        else:
+            i += 1
 
-a = '( there are = sometimes  \n b = 5 )'
-b = re.split('(\s+|\=|\(|\))', a)
-print(b) 
+def parse(tokens):
+    a = 5
 
-c = 'a  b c d e'
-d = re.split('\s+', c)
-print(d)
+my_file = open('test.txt', 'r')
+program = my_file.read()
+my_tokens = tokenize(program)
+print(my_tokens)
+
+a = [1, 2, 3, 4]
+
