0a4acc616a0d4e837a0b25458a77c47573e4575c
Kevin Jin <kjin2010@gmail.com>
2020-06-14 16:15:57 -0500

    fixed parse and tokenize

--- a_lab/lab.py
+++ a_lab/lab.py
@@ -1,3 +1,6 @@
+class EvaluationError(Exception):
+    pass
+
 # splits a program into its tokens 
 def tokenize(program):
     # split program into lines to deal with comments 
@@ -6,48 +9,56 @@ def tokenize(program):
     for cur_index, line in enumerate(program_lines):
         if ';' in line:
             program_lines[cur_index] = line[:line.index(';')]
-    single_program = ''.join(program_lines).replace('(', ' ( ').replace(')', ' ) ')
-    return single_program.split()
+    single_program = ' '.join(program_lines)
+    return single_program.replace('(', ' ( ').replace(')', ' ) ').split()
 
-def parse(tokens):
-    # make sure expression exists
-    if tokens:
-        initial_token = tokens.pop(0)
-    else:
-        return
+def is_valid_parse(tokens):
+    open_p = 0
+    for token in tokens:
+        if token == '(':
+            open_p += 1
+        elif token == ')':
+            open_p -= 1
+        if open_p < 0:
+            return False
+    return open_p == 0
 
-    # expressions have to start with parentheses
-    if initial_token != '(':
-        raise SyntaxError('missing opening parentheses; invalid program')
-    # parsed representation of tokens
-    cur_parse = []
-    # continue if more tokens exist and is not end of expression
-    while tokens and tokens[0] != ')':
-        # append parsed subexpression 
+def parsing(tokens):
+    initial_token = tokens.pop(0)
+    if not tokens:
+        try:
+            if '.' in initial_token:
+                cur_num = float(initial_token)
+            else:
+                cur_num = int(initial_token)
+            return cur_num
+        except:
+            return initial_token
+
+    cur_list = []
+    while tokens[0] != ')':
         if tokens[0] == '(':
-            cur_parse.append(parse(tokens))
-        # append current token
-        else: 
+            cur_list.append(parsing(tokens))
+        else:
             cur_token = tokens.pop(0)
             try:
                 if '.' in cur_token:
                     cur_num = float(cur_token)
                 else:
                     cur_num = int(cur_token)
-                cur_parse.append(cur_num)
+                cur_list.append(cur_num)
             except:
-                cur_parse.append(cur_token)
+                cur_list.append(cur_token)
+    tokens.pop(0)
+    return cur_list
 
-    # if empty list, missing close parentheses
-    if not tokens:
-        raise SyntaxError('missing closing parentheses; invalid program')
-    # remove it and return current parsed expression
-    else:
-        tokens.pop(0)
-        return cur_parse
+def parse(tokens):
+    if not is_valid_parse(tokens):
+        raise SyntaxError
+    return parsing(tokens)
 
 '''
-BUILT-IN FUNCTION DEFINITIONS
+~~~~~~~~~~~~~~~~~~~~~~~~~ BUILT-IN FUNCTION DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 
 def sub(args):
@@ -79,49 +90,110 @@ carlae_builtins = {
 }
 
 ''' 
-HELPER CLASSES
+~~~~~~~~~~~~~~~~~~~~~~~~~ HELPER CLASSES ~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 
 class Environment():
-    def __init__(self, symbols = carlae_builtins):
-        self.symbols = symbols
+    def __init__(self, parent = carlae_builtins):
+        self.parent = parent
+        self.symbols = {}
 
+    def __setitem__(self, key, val):
+        self.symbols[key] = val
+    
+    def __getitem__(self, key):
+        # if key in current environment
+        if key in self.symbols:
+            return self.symbols[key]
+        # if key in carlae_builtins
+        elif key in carlae_builtins:
+            return carlae_builtins[key]
+        # if not in parents
+        elif parent == carlae_builtins:
+            raise EvaluationError
+        # check parents 
+        return self.parent[key]
 
-def evaluate(parsed):
-    # error
+    def set(self, key, value):
+        if key not in self.symbols:
+            a = 5
+
+def is_valid_eval(parsed):
+    a = 6
+
+def result_and_env(parsed, env = None):
+    if not env:
+        env = Environment()
+
+    # empty list means no program run
     if parsed == []:
         raise EvaluationError
 
-    # error
+    # single value in expression [1 1.0]
     if isinstance(parsed, list) and (isinstance(parsed[0], int) or isinstance(parsed[0], float)):
         raise EvaluationError 
 
-    # single value
-    if isinstance(parsed, int) or isinstance(parsed, float):
-        return parsed
+    # binding that doesn't exist
+    if isinstance(parsed, str) and parsed not in env.symbols:
+        raise EvaluationError
 
+    # single value not in expression
+    if isinstance(parsed, int) or isinstance(parsed, float):
+        return parsed, env
+
+    # binding 
+    if isinstance(parsed, str):
+        return env[parsed], env
+
+
+    # executing operations
     operation = parsed[0]
+
+    if operation == 'define':
+        env[parsed[1]] = result_and_env(parsed[2], env)[0]
+        return env[parsed[1]], env
+
     operands = []
+    # evaluate subsequent expressions to perform function on
     for expression in parsed[1:]:
-        operands.append(evaluate(expression))
-    if operation in carlae_builtins.keys():
-        return carlae_builtins[operation](operands)
+        operands.append(evaluate(expression, env))
+    if operation in carlae_builtins:
+        return carlae_builtins[operation](operands), env
     else:
         print('undefined operation')
 
+
+def evaluate(parsed, env = None):
+    return result_and_env(parsed, env)[0]
+
+
+'''
+# for repl/testing 
 if __name__ == '__main__':
+    env = Environment()
+    file_name = 'test.txt'
+    file = open(file_name, 'r')
+    file_program = file.read()
+    # print('test output: ', evaluate(parse(tokenize(file_program))), env)
+
     while True:
         try:
             program = input('in:\n')
             if program == 'quit':
                 break
             else:
-                print('\nout:\n', evaluate(parse(tokenize(program))))
+                # value = evaluate(parse(tokenize(program)), env)
+                value = parse(tokenize(program))
+                print('out:\n', value, '\n')
         except:
-            print('\nout:\ninvalid program\n')
+            print('out:\ninvalid program\n')
+
+'''
 
 
-
+# program = open('test.txt').read()
+# print'original program: ', program
+# print evaluate(parse(tokenize(program)))
 
 
 
